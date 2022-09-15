9255165c4e65184c9925d7b89bf3049efd6b68a0
Mickyokta <micky.okta36@gmail.com>
2022-09-15 11:05:22 +0700

    get third party data

--- router/index.js
+++ router/index.js
@@ -1,8 +1,8 @@
 const Controller = require("../controllers/controller");
 const router = require('express').Router()
-const videos = require('./videos')
+
 
 router.get('/splitgate', Controller.getSplitgate)
-router.use('/videos', videos)
+
 
 module.exports = router
